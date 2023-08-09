c838defeb259aa36727d36d7cc859570ae127da4
rsv6 <rafaelsvbr@gmail.com>
2023-08-09 01:51:56 -0300

    add: jwt.sign route singIn

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -3,11 +3,13 @@ export class User {
     id? : Number;
     name? : string;
     login? : string;
+    email? : string;
     password? : string;
 
-    constructor(name: string, login: string, password: string) {
+    constructor(name: string, login: string, email: string, password: string) {
         this.name = name;
         this.login = login;
+        this.email = email;
         this.password = password;
     }
 }
--- src/interfaces/repositories/UserRepository.ts
+++ src/interfaces/repositories/UserRepository.ts
@@ -1,4 +1,5 @@
 import { userMock } from "../../adapters/database/mock";
+import { JwtAuth } from "../../application/services/JwtAuth";
 import { User } from "../../domain/entities/User";
 
 export class UserRepositoy {
@@ -7,8 +8,27 @@ export class UserRepositoy {
     private static usuariosMock = userMock;
 
     register(user: User) {
+
         UserRepositoy.ListUsers.push(user);
         return true;
+    }
+
+    signIn(login: string, password: string) {
+        let users = this.findAll().some(user => user.login == login && user.password == password);
+
+        console.log("User: ", users);
+
+        if (!users) return null;
+
+        const token = new JwtAuth().generateAcessToken({ login, password })
+
+        console.log("Token: ", token)
+
+        if (!token) {
+            return null;
+        }
+
+        return token;
     }
 
     findAll(): User[] {
